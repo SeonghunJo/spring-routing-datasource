374999a13d096124a9d78fe3f3b9c48d5b7a2a7f
조성훈 <jsh1026@midasit.com>
2020-02-10 16:37:45 +0900

    Aspect 빈으로 등록

--- src/main/java/com/midasin/jta/aspect/RoutingDataSourceAspect.java
+++ src/main/java/com/midasin/jta/aspect/RoutingDataSourceAspect.java
@@ -5,9 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
+@Component
 public class RoutingDataSourceAspect {
 
     @Around("execution(* com.midasin.jta..service.*Service.*(..))")
